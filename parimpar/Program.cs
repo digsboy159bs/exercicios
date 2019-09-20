40ab7ce307484a5ae657355717590ed8409a1b9e
digsboy159bs <34436810852@info.local>
2019-09-20 17:28:45 -0300

    exercios while

---
+++ parimpar/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace parimpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         int numero=0;
+
+            do{
+                  Console.Write("digite um numero:");
+                numero =int.Parse(Console.ReadLine());
+
+               
+                
+               
+
+          
+              
+                if(numero % 2 == 0){
+                Console.WriteLine("o numero é par");
+                }
+                else
+                Console.WriteLine("o numero é impar" );
+
+            }while(numero!= 0);
+              
+              }
+             
+
+        }
+    }
+
---
+++ parimpar2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace parimpar2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+        for (int i=1; i<100; i+=2){
+            Console.Write(i+ " ");
+
+        }
+
+        
+        
+        }
+    }
+
+}
