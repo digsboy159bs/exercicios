2f650bdbe25eeb97e55f54c599e48d431ea4dc27
digsboy159bs <34436810852@info.local>
2019-09-26 17:16:31 -0300

    foreach

---
+++ exmatris/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace exmatris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double maior= 0;
+               double menor= 0;
+             double[] temperatura = new double[12];
+          for(int i = 0; i<12; i++){
+             Console.Write($"digite a temperatura do mes {i+ 1}: ");
+             temperatura[i]= double.Parse(Console.ReadLine());
+          }
+          maior = temperatura[0];
+          menor = temperatura[0];
+
+          foreach(double temp in temperatura){
+              if(temp > maior){
+                  maior= temp;
+              }else if(temp < menor){
+                  menor = temp;
+              }
+
+          }
+          Console.WriteLine($"a maior temperatura é {maior}");
+           Console.WriteLine($"a menor temperatura é {menor}");
+        }
+    }
+}
---
+++ triangulos/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace triangulos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         int[] vetor = new int[6];
+         int par = 0;
+         int impar = 0;
+
+        for(int cont = 0; cont <6; cont++){
+            Console.Write($"digite o {cont+1} número: ");
+            vetor [cont] = int.Parse(Console.ReadLine());
+        }
+        foreach(int num in vetor){
+            if(num % 2 == 0){
+                par++;
+
+            }else {
+                impar++;
+
+        }
+        }
+        Console.WriteLine($"voce tem {par} numeros pares e {impar} numeros impares");
+
+
+
+
+
+        }
+    }
+}
